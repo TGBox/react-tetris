e783ac0741e85b9a031e468c741fbd5ce0d2135c
Dani <daniel.roesch91@gmail.com>
2022-06-06 21:19:35 +0200

    Row completion removal added.

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -4,8 +4,25 @@ import { createStage } from "../gameHelpers";
 // Hook to handle the useStage for the stage component in the game.
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
+  const [rowsCleared, setRowsCleared] = useState(0);
 
   useEffect(() => {
+    setRowsCleared(0);
+
+    // Handles deleting of filled rows an refilling of the stage array.
+    const sweepRows = newStage => {
+      // Checks if any cell in the row from our stage is 0. If not, it counts our score up and readds a row.
+      newStage.reduce((accumulated, row) => {
+        if(row.findIndex(cell => cell[0] === 0) === -1) {
+          setRowsCleared(prev => prev + 1);
+          accumulated.unshift(new Array(newStage[0].length).fill([0, "clear"]));
+          return accumulated;
+        }
+        accumulated.push(row);
+        return accumulated;
+      }, []);
+    }
+
     // Is inside of the useEffect because we won't use it somewhere else.
     const updateStage = prevStage => {
       // First we will flush the stage from the previous render.
@@ -25,9 +42,10 @@ export const useStage = (player, resetPlayer) => {
         });
       });
 
-      // Then we check for collision.
+      // Then we check for collision. If true, we reset the player and sweeps the rows of the new stage.
       if(player.collided) {
         resetPlayer();
+        return sweepRows(newStage);
       }
 
       return newStage;
@@ -36,5 +54,5 @@ export const useStage = (player, resetPlayer) => {
     setStage(prev => updateStage(prev));
   }, [player.pos.x, player.pos.y, player.collided, player.tetrisblocks, resetPlayer]);
 
-  return [stage, setStage];
+  return [stage, setStage, rowsCleared];
 };
